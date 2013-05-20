6e97e7ebea4a55d526b0f6a99bbad35fb8c29b9b
ljj <ljj@purple.cbs.dtu.dk>
2013-05-20 11:01:03 +0200

    Added swipe support for paging on touch devices!

--- blackmamba/html.py
+++ blackmamba/html.py
@@ -311,6 +311,7 @@ class XHead(XTag):
 		self.css = []
 		self.scripts = []
 		self.scripts.append("https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js")
+		self.scripts.append("/scripts/hammer.min.js")
 		self.scripts.append("/scripts/blackmamba.js")
 		self.scripts.append("/scripts/base64.js")
 		
