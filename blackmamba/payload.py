b1e9d1ff965fb274f58d49461917b9cfd27cf542
Lars Juhl Jensen <11190105+larsjuhljensen@users.noreply.github.com>
2014-04-07 12:46:35 -0300

    Initial implementation of STRING payload extension API.

--- blackmamba/payload.py
+++ blackmamba/payload.py
@@ -7,6 +7,22 @@ import xpage
 import mamba.task
 
 
+def string_entity(entity):
+	type_id = {
+		3702 : "AT[1-5CM]G[0-9]{5}",
+		4932 : "Y[A-P][LR][0-9]{3}[WC]|Q[0-9]{4}",
+		7227 : "FBpp",
+		9606 : "ENSP",
+		10090 : "ENSMUSP",
+		10116 : "ENSRNOP"
+	}
+	tokens = entity.split(".")
+	if int(tokens[0]) in type_id:
+		return re.match(type_id[int(tokens[0])], ".".join(tokens[1:]))
+	else:
+		return True
+
+
 class StringNetwork(mamba.task.Request):
 	
 	def main(self):
@@ -49,16 +65,20 @@ class StringPayload(mamba.task.Request):
 	def main(self):
 		action = self.http.get_action()
 		design = xpage.get_design()
+		host = self.http.headers["Host"]
 		if action == "StringPayload":
-			json = '''{"edges_webservice":"http://%s/StringPayloadEdges","name":"%s"}''' % (self.http.headers["Host"], design["TITLE"])
+			json = '''{"edges_webservice":"http://%s/StringPayloadEdges","extension_nodes_webservice":"http://%s/StringPayloadExtensionNodes","name":"%s"}''' % (host, host, design["TITLE"])
 		elif re.match("-?[0-9]+\.", action):
-			json = '''{"nodes_webservice":"http://%s/StringPayloadEntity/%s","name":"%s"}''' % (self.http.headers["Host"], action, design["TITLE"])
+			json = '''{"nodes_webservice":"http://%s/StringPayloadEntity/%s","name":"%s"}''' % (host, action, design["TITLE"])
 		else:
-			json = '''{"nodes_webservice":"http://%s/StringPayloadFigure/%s","name":"%s"}''' % (self.http.headers["Host"], action, design["TITLE"])
+			json = '''{"nodes_webservice":"http://%s/StringPayloadFigure/%s","name":"%s"}''' % (host, action, design["TITLE"])
 		mamba.http.HTTPResponse(self, json, "application/json").send()
 
 
 class StringPayloadEdges(mamba.task.Request):
+	
+	def validate_edge(self, entity1, entity2):
+		return string_entity(entity1) and string_entity(entity2)
 	
 	def main(self):
 		rest = mamba.task.RestDecoder(self)
@@ -68,17 +88,23 @@ class StringPayloadEdges(mamba.task.Request):
 			only_internal = bool(rest["only_internal"])
 		if "identifiers" in rest:
 			entities = rest["identifiers"].split(" ")
-			for evidence in ["knowledge", "textmining", "predictions"]:
+			for evidence in ["knowledge", "experiments", "textmining", "predictions"]:
 				connection = database.Connect(evidence)
 				for entity1 in entities:
 					qtype, qid = entity1.split(".", 1)
 					sql = "SELECT id2, score FROM pairs WHERE type1=%d AND id1='%s' AND type2=%d AND score >= 0.5" % (int(qtype), pg.escape_string(qid), int(qtype))
 					for row in connection.query(sql).getresult():
 						entity2 = "%s.%s" % (qtype, row[0])
-						if (entity2 in entities) == only_internal:
+						if validate_edge(entity1, entity2) and entity2 in entities or not only_internal:
 							score = 0.9*row[1]/5
 							tsv.append("%s\t%s\t%s\t%f\n" % (entity1, entity2, evidence, score))
 		mamba.http.HTTPResponse(self, "".join(tsv), "text/plain").send()
+
+
+class StringPayloadExtensionEdges(StringPayloadEdges):
+	
+	def validate_edge(self, entity1, entity2):
+		return not string_entity(entity1) or not string_entity(entity2)
 
 
 class StringPayloadNodes(mamba.task.Request):
@@ -89,6 +115,9 @@ class StringPayloadNodes(mamba.task.Request):
 	def get_links(self, entities):
 		return {}
 	
+	def validate_node(self, entity):
+		return string_entity(entity)
+	
 	def main(self):
 		rest = mamba.task.RestDecoder(self)
 		tsv = []
@@ -97,14 +126,21 @@ class StringPayloadNodes(mamba.task.Request):
 			colors = self.get_colors(entities)
 			links = self.get_links(entities)
 			for entity in entities:
-				color = ""
-				if entity in colors:
-					color = colors[entity]
-				link = ""
-				if entity in links:
-					link = links[entity]
-				tsv.append("%s\t%s\t\t%s\n" % (entity, color, link))
+				if validate_node(entity):
+					color = ""
+					if entity in colors:
+						color = colors[entity]
+					link = ""
+					if entity in links:
+						link = links[entity]
+					tsv.append("%s\t%s\t\t%s\n" % (entity, color, link))
 		mamba.http.HTTPResponse(self, "".join(tsv), "text/plain").send()
+
+
+class StringPayloadExtensionNodes(StringPayloadNodes):
+	
+	def validate_node(self, entity):
+		return not string_entity(entity)
 
 
 class StringPayloadEntity(StringPayloadNodes):
@@ -130,7 +166,7 @@ class StringPayloadEntity(StringPayloadNodes):
 	def get_scores(self, entities):
 		scores = {}
 		qtype2, qid2 = self.http.get_action().split(".", 1)
-		for evidence in ["knowledge", "textmining", "predictions"]:
+		for evidence in ["knowledge", "experiments", "textmining", "predictions"]:
 			connection = database.Connect(evidence)
 			if connection != None:
 				for entity in entities:
