d364ed2448c839ba7960f5431bbf2c532e9947b9
larsjuhljensen <larsjuhljensen@localhost>
2017-01-17 22:48:49 +0100

    Improvements to BeCalm support to better use queues.

--- reflect/becalm.py
+++ reflect/becalm.py
@@ -11,106 +11,123 @@ import mamba.util
 
 
 class BeCalm(mamba.task.Request):
+
+	def download(self):
+		abstracts = []
+		articles = []
+		patents = []
+		for document in self.documents:
+			source = document["source"]
+			if source == "PATENT SERVER":
+				patents.append(document["document_id"])
+			elif source == "PMC":
+				articles.append(document["document_id"])
+			elif source == "PUBMED":
+				abstracts.append(document["document_id"])
+		self.sections = []
+		for i in xrange(0, len(abstracts), 1000):
+			cmd = '''curl --silent --data 'db=pubmed&id=%s&rettype=medline&retmode=text' https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi''' % ",".join(abstracts[i:i+1000])
+			rc, so, se = mamba.util.Command(cmd).run()
+			pmid = None
+			for line in so.replace("\n     ","").splitlines():
+				linetype = line[0:5]
+				if linetype == "PMID-":
+					pmid = line[5:]
+				elif linetype == "TI  -":
+					if pmid is not None:
+						self.sections.append([pmid, "T", line[5:]])
+				elif linetype == "AB  -":
+					if pmid is not None:
+						self.sections.append([pmid, "A", line[5:]])
+		for i in xrange(0, len(patents), 1000):
+			cmd = '''curl --silent -H "Content-Type: application/json" --data '{"patents": %s}' http://193.147.85.10:8087/patentserver/tsv''' % json.dumps(patents[i:i+1000])
+			rc, so, se = mamba.util.Command(cmd).run()
+			for line in so.splitlines():
+				columns = line.split("\t", 2)
+				self.sections.append([columns[0], "T", columns[1]])
+				if len(columns) == 3:
+					 self.sections.append([columns[0], "A", columns[2]])
+		self.queue("tagging")
 	
+	def tagging(self):
+		type_map = {
+			-1 : "CHEMICAL",
+			-2 : "ORGANISM",
+			-22 : "SUBCELLULAR_STRUCTURE",
+			-25 : "TISSUE_AND_ORGAN",
+			-26 : "DISEASE"
+		}
+		results = []
+		for section in self.sections:
+			matches = mamba.setup.config().tagger.get_matches(document=section[2], document_id=0, entity_types=self.entity_types, auto_detect=self.auto_detect)
+			for match in matches:
+				init, end, entities = match
+				if self.disambiguate and len(entities) > 1:
+					continue
+				result = {}
+				result["document_id"] = section[0]
+				result["section"] = section[1]
+				result["init"] = init
+				result["end"] = end
+				result["annotated_text"] = section[2][init:end]
+				types = set()
+				database_ids = []
+				for entity in entities:
+					if entity[0] > 0:
+						types.add("GENE")
+						database_ids.append("%d.%s" % (entity[0], entity[1]))
+					elif entity[0] in type_map:
+						types.add(type_map[entity[0]])
+						database_ids.append(entity[1])
+					else:
+						types.add("unknown")
+						database_ids.append(entity[1])
+				if len(types) == 1:
+					result["type"] = "".join(types)
+				else:
+					result["type"] = "unknown"
+				result["database_id"] = ",".join(database_ids)
+				results.append(result)
+		cmd = "curl --silent --data @- '%s/saveAnnotations/JSON?apikey=%s&communicationId=%s'" % (self.apiurl, self.apikey, self.communication_id)
+		devnull = open(os.devnull, "w")
+		pipe = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=devnull, stderr=subprocess.STDOUT)
+		pipe.stdin.write(json.dumps(results))
+		pipe.stdin.close()
+
 	def main(self):
 		rest = mamba.task.RestDecoder(self)
 		input = json.loads(self.http.body)
-		apikey = input["custom_parameters"]["apikey"]
-		apiurl = input["custom_parameters"]["apiurl"]
+		self.apikey = input["custom_parameters"]["apikey"]
+		self.apiurl = input["custom_parameters"]["apiurl"]
+		self.cache = True
+		if "cache" in input["custom_parameters"]:
+			self.cache = input["custom_parameters"]["cache"]
+		self.disambiguate = True
+		if "disambiguate" in input["custom_parameters"]:
+			self.disambiguate = input["custom_parameters"]["disambiguate"]
 		method = input["method"]
 		if method == "getState":
-			output = '''{"status":200,"success":true,"key":"%s","data":{"state":"Running","version":"0.4","version_changes":"Use POST for saveAnnotations","max_analyzable_documents":100000}}''' % apikey
-			mamba.http.HTTPResponse(self, output).send()
+			mamba.http.HTTPResponse(self, '''{"status":200,"success":true,"key":"%s","data":{"state":"Running","version":"0.5","version_changes":"Better use of queue.","max_analyzable_documents":100000}}''' % self.apikey).send()
 		elif method == "getAnnotations":
-			commid = input["parameters"]["communication_id"]
-			output = '''{"status": 200,"success": true,"key":"%s"}''' % apikey
-			mamba.http.HTTPResponse(self, output).send()
-			communication_id = input["parameters"]["communication_id"]
+			mamba.http.HTTPResponse(self, '''{"status": 200,"success": true,"key":"%s"}''' % self.apikey).send()
+			self.communication_id = input["parameters"]["communication_id"]
+			self.entity_types = []
+			self.auto_detect = False
 			types = input["parameters"]["types"]
-			entity_types = []
-			auto_detect = False
 			if "GENE" in types:
-				entity_types.append(9606)
-				auto_detect = True
+				self.entity_types.append(9606)
+				self.auto_detect = True
 			if "CHEMICAL" in types:
-				entity_types.append(-1)
+				self.entity_types.append(-1)
 			if "ORGANISM" in types:
-				entity_types.append(-2)
+				self.entity_types.append(-2)
 			if "SUBCELLULAR_STRUCTURE" in types:
-				entity_types.append(-22)
+				self.entity_types.append(-22)
 			if "TISSUE_AND_ORGAN" in types:
-				entity_types.append(-25)
+				self.entity_types.append(-25)
 			if "DISEASE" in types:
-				entity_types.append(-26)
-			type_map = {
-				-1 : "CHEMICAL",
-				-2 : "ORGANISM",
-				-22 : "SUBCELLULAR_STRUCTURE",
-				-25 : "TISSUE_AND_ORGAN",
-				-26 : "DISEASE"
-			}
-			documents = input["parameters"]["documents"]
-			abstracts = []
-			articles = []
-			patents = []
-			for document in documents:
-				source = document["source"]
-				sections = []
-				if source == "PATENT SERVER":
-					patents.append(document["document_id"])
-				elif source == "PMC":
-					articles.append(document["document_id"])
-				elif source == "PUBMED":
-					abstracts.append(document["document_id"])
-			for i in xrange(0, len(abstracts), 1000):
-				cmd = '''curl --silent --data 'db=pubmed&id=%s&rettype=medline&retmode=text' https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi''' % ",".join(abstracts[i:i+1000])
-				rc, so, se = mamba.util.Command(cmd).run()
-				pmid = None
-				for line in so.replace("\n     ","").splitlines():
-					linetype = line[0:5]
-					if linetype == "PMID-":
-						pmid = line[5:]
-					elif linetype == "TI  -":
-						if pmid is not None:
-							sections.append([pmid, "T", line[5:]])
-					elif linetype == "AB  -":
-						if pmid is not None:
-							sections.append([pmid, "A", line[5:]])
-			for i in xrange(0, len(patents), 1000):
-				cmd = '''curl --silent -H "Content-Type: application/json" --data '{"patents": %s}' http://193.147.85.10:8087/patentserver/tsv''' % json.dumps(patents[i:i+1000])
-				rc, so, se = mamba.util.Command(cmd).run()
-				for line in so.splitlines():
-					columns = line.split("\t", 2)
-					sections.append([columns[0], "T", columns[1]])
-					if len(columns) == 3:
-						 sections.append([columns[0], "A", columns[2]])
-			results = []
-			for section in sections:
-				matches = mamba.setup.config().tagger.get_matches(document=section[2], document_id=0, entity_types=entity_types, auto_detect=auto_detect)
-				for match in matches:
-					init, end, entities = match
-					if len(entities) > 1:
-						continue
-					for entity in entities:
-						result = {}
-						result["document_id"] = section[0]
-						result["section"] = section[1]
-						result["init"] = init
-						result["end"] = end
-						result["annotated_text"] = section[2][init:end]
-						if entity[0] > 0:
-							result["type"] = "GENE"
-							result["database_id"] = "%d.%s" % (entity[0], entity[1])
-						elif entity[0] in type_map:
-							result["type"] = type_map[entity[0]]
-							result["database_id"] = entity[1]
-						else:
-							result["type"] = "unknown"
-							result["database_id"] = entity[1]
-						results.append(result)
-			cmd = "curl --silent --data @- '%s/saveAnnotations/JSON?apikey=%s&communicationId=%s'" % (apiurl, apikey, communication_id)
-			pipe = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
-			pipe.stdin.write(json.dumps(results))
-			pipe.stdin.close()
+				self.entity_types.append(-26)
+			self.documents = input["parameters"]["documents"]
+			self.queue("download")
 		else:
 			mamba.http.HTTPErrorResponse(self, 400, "Request contains an unsupported BeCalm method").send()
